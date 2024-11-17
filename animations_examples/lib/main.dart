250b4bee9c9ab3296155954b3c0b642c21d565af
SamedHrmn <samed_harman@hotmail.com>
2024-11-17 22:42:37 +0300

    add elastic line animation

--- animations_examples/lib/main.dart
+++ animations_examples/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animations_examples/view/elastic_line_view.dart';
 import 'package:flutter/material.dart';
 
 import 'view/animated_container_view.dart';
@@ -24,19 +25,30 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Material App Bar'),
+        title: Text('Flutter Animation Samples'),
       ),
-      body: Column(
-        children: [
-          Flexible(
-            child: Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => AnimatedContainersView(),
-                        )),
-                    child: Text("Animated Containers"))),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AnimatedContainersView(),
+                ),
+              ),
+              child: Text("Animated Containers"),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => ElasticLineView(),
+                ),
+              ),
+              child: Text("Elastic Line"),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ animations_examples/lib/view/elastic_line_view.dart
@@ -0,0 +1,187 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ElasticLineView extends StatefulWidget {
+  const ElasticLineView({super.key});
+
+  @override
+  State<ElasticLineView> createState() => _ElasticLineViewState();
+}
+
+class _ElasticLineViewState extends State<ElasticLineView> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  final Offset _pointA = Offset(0, 0);
+  late final Offset _pointB;
+  Offset _grabPoint = Offset.zero;
+  Offset _currentGrabPoint = Offset.zero;
+  Offset _initialGrabPoint = Offset.zero;
+  bool _isDragging = false;
+  bool _isInitializePoints = false;
+  final double _lineTapOffset = 50;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = _controller.drive(
+      Tween<double>(begin: 0.0, end: 1.0).chain(
+        CurveTween(curve: Curves.easeOut),
+      ),
+    );
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _pointB = MediaQuery.sizeOf(context).bottomRight(_pointA);
+        _isInitializePoints = true;
+      });
+    });
+  }
+
+  void _startSpringAnimation() {
+    _initialGrabPoint = _currentGrabPoint;
+    _controller.reset();
+    _controller.forward();
+  }
+
+  bool _isTapNearLine(Offset tapPoint) {
+    final Offset closestPoint = _getClosestPointOnLine(tapPoint);
+    return (closestPoint - tapPoint).distance < _lineTapOffset;
+  }
+
+  Offset _getClosestPointOnLine(Offset tapPoint) {
+    final Offset lineVector = _pointB - _pointA;
+    final Offset tapVector = tapPoint - _pointA;
+
+    final double projectionFactor = (tapVector.dx * lineVector.dx + tapVector.dy * lineVector.dy) / (lineVector.dx * lineVector.dx + lineVector.dy * lineVector.dy);
+
+    return Offset(
+      _pointA.dx + lineVector.dx * projectionFactor,
+      _pointA.dy + lineVector.dy * projectionFactor,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: !_isInitializePoints
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GestureDetector(
+              onPanStart: (details) {
+                if (_isTapNearLine(details.localPosition)) {
+                  _controller.reset();
+                  _isDragging = true;
+                  _grabPoint = _getClosestPointOnLine(details.localPosition);
+                  _currentGrabPoint = _grabPoint;
+                }
+              },
+              onPanUpdate: (details) {
+                if (_isDragging) {
+                  setState(() {
+                    _currentGrabPoint += details.localPosition - _currentGrabPoint;
+                  });
+                }
+              },
+              onPanEnd: (_) {
+                if (_isDragging) {
+                  _isDragging = false;
+                  _startSpringAnimation();
+                }
+              },
+              child: CustomPaint(
+                painter: ElasticLinePainter(
+                  pointA: _pointA,
+                  pointB: _pointB,
+                  grabPoint: _grabPoint,
+                  currentGrabPoint: _currentGrabPoint,
+                  initialGrabPoint: _initialGrabPoint,
+                  animationValue: _animation.value,
+                  isDragging: _isDragging,
+                ),
+                child: Container(),
+              ),
+            ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class ElasticLinePainter extends CustomPainter {
+  final Offset pointA;
+  final Offset pointB;
+  final Offset grabPoint;
+  final Offset currentGrabPoint;
+  final Offset initialGrabPoint;
+  final double animationValue;
+  final bool isDragging;
+  final double oscillationFactor;
+  final double dampingFactor;
+
+  ElasticLinePainter({
+    required this.pointA,
+    required this.pointB,
+    required this.grabPoint,
+    required this.currentGrabPoint,
+    required this.initialGrabPoint,
+    this.oscillationFactor = 10,
+    this.dampingFactor = -4,
+    required this.animationValue,
+    required this.isDragging,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 4.0
+      ..style = PaintingStyle.stroke;
+
+    final double damping = exp(dampingFactor * animationValue);
+    final double oscillation = sin(animationValue * oscillationFactor * pi);
+
+    final Offset animatedGrabPoint = isDragging
+        ? currentGrabPoint
+        : Offset.lerp(
+              initialGrabPoint,
+              grabPoint,
+              animationValue,
+            )! +
+            Offset(
+              oscillation * damping * (grabPoint.dx - initialGrabPoint.dx),
+              oscillation * damping * (grabPoint.dy - initialGrabPoint.dy),
+            );
+
+    final path = Path()
+      ..moveTo(pointA.dx, pointA.dy)
+      ..quadraticBezierTo(
+        animatedGrabPoint.dx,
+        animatedGrabPoint.dy,
+        pointB.dx,
+        pointB.dy,
+      );
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
